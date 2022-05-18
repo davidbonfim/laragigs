a4a86e2e9cf6ca6d6f28116da5ab770ffc72ce37
David Bonfim <84916984+davidbonfim@users.noreply.github.com>
2022-05-18 12:53:02 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('listings', [
+        "heading" =>"Latest Listings",
+        "listings" => [
+            [
+                "id" => 1,
+                "title" => "Listings 1",
+                "description" =>"It is a long established fact that a reader will 
+                be distracted by the readable content of a page when looking at its layout.
+                 The point of using Lorem Ipsum is that it has a more-or-less normal distribution
+                of letters, as opposed to using 'Content here, content here', making it look like
+                readable English. Many desktop publishing packages and web page editors now use
+                Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover
+                many web sites still in their infancy. Various versions have evolved over the years, 
+                sometimes by accident, sometimes on purpose (injected humour and the like)."
+            ],
+            [
+                "id" => 2,
+                "title" => "Listings 2",
+                "description" =>"It is a long established fact that a reader will 
+                be distracted by the readable content of a page when looking at its layout.
+                 The point of using Lorem Ipsum is that it has a more-or-less normal distribution
+                of letters, as opposed to using 'Content here, content here', making it look like
+                readable English. Many desktop publishing packages and web page editors now use
+                Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover
+                many web sites still in their infancy. Various versions have evolved over the years, 
+                sometimes by accident, sometimes on purpose (injected humour and the like)."
+            ]
+        ]
+    ]);
+});
+
